ea3bfe352894ac35f362821643f95bde29a276e6
sjl8255 <la.sandrajose98@gmail.com>
2020-12-07 20:55:35 +0530

    changes made to guessinggame.sh

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Guessing Game
-Mon Dec 7 18:09:00 SLST 2020
-The number of lines in the code is 18
+Mon Dec 7 20:54:15 SLST 2020
+The number of lines in the code is 29
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,6 @@
 check_value=$(ls -al | grep "^-" | wc -l)
 echo "How many files are available in this directory?"
+diff=5
 while true
 do
 echo "Take a guess: "
@@ -10,9 +11,19 @@ echo "You got the correct number!"
 exit
 elif [ $choice -gt $check_value ]
 then
+if [ `expr $choice - $check_value` -le 5 ]
+then
+echo "The number is close but still high"
+else
 echo "The number chosen is too high"
+fi
 elif [ $choice -lt $check_value ]
 then
+if [ `expr $check_value - $choice` -le 5 ]
+then
+echo "The number is close but still low"
+else
 echo "The number chosen is too low"
+fi
 fi
 done
